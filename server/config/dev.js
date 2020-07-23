c9d39219450225175a4cb00b01d30b35651116de
MitchAhoy <51808358+MitchAhoy@users.noreply.github.com>
2020-07-23 21:53:42 +1000

    restructure

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '884288237438-ri6bjm0o2ggt0enfc7485bkl0jodq0f3.apps.googleusercontent.com',
+    googleClientSecret: 'YZe05jKjjgKjw8VBoWAytwWH',
+    mongoURI: 'mongodb+srv://mitch:XiyK4qBa9P7ViffT@emaily.3ckpq.mongodb.net/newdb?retryWrites=true&w=majority',
+    cookieKey: 'klngdkngjkndlfdkfsjfkijdivjdnfvndfvnsijvndifjnvidfnv'
+}

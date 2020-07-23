05b0f1e4d3c972585c217dd25989c2f68e161fe0
MitchAhoy <51808358+MitchAhoy@users.noreply.github.com>
2020-07-23 22:42:02 +1000

    create-react-app and proxy

---
+++ server/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+     <h2><a href="./auth/google">Sign-In with Google</a></h2>
+    </div>
+  );
+}
+
+export default App;
